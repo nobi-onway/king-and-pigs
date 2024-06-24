9a88d76b6bef5e0d90906d53a701832982e87133
duythu1 <75884354+duythu3605@users.noreply.github.com>
2024-06-25 01:08:14 +0700

    aimming
    add script aimming 0 -90 degree

---
+++ Assets/Scripts/Skills/Aiming.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiming : MonoBehaviour
+{
+    [SerializeField] private Transform FirePosition;
+
+    [SerializeField] private float _minAngleZ;
+    [SerializeField] private float _maxAngleZ;
+    [SerializeField] private float _speedRotate;
+    [SerializeField] private bool _isAimingUp = true;
+    
+
+    [SerializeField] private InputManager inputManager;
+
+    private Coroutine _rotationCoroutine;
+
+    private void Start()
+    {
+        inputManager.OnPointerDown  += ()=> _rotationCoroutine = StartCoroutine(RotateCoroutine());
+        inputManager.OnPointerUp += () => StopCoroutine(_rotationCoroutine);
+    }
+
+
+
+    IEnumerator RotateCoroutine()
+    {
+        while (true)
+        {
+            int direc = _isAimingUp ? 1 : -1; 
+            FirePosition.Rotate(0, 0, _speedRotate  * Time.deltaTime * direc);
+            if (FirePosition.rotation.eulerAngles.z >= _maxAngleZ && _isAimingUp) _isAimingUp = false;
+            else if (FirePosition.rotation.eulerAngles.z <= _minAngleZ) _isAimingUp = true;
+            
+            yield return new WaitForEndOfFrame();
+        }
+        
+    }
+
+}
