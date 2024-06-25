d1a219552d9b8899cafe073ec469a4b162383f0a
duythu1 <75884354+duythu3605@users.noreply.github.com>
2024-06-25 16:38:27 +0700

    update heal and attackdamage
    duythu

--- Assets/Scripts/Skills/DestroyOnBouncy.cs
+++ Assets/Scripts/Skills/DestroyOnBouncy.cs
@@ -15,10 +15,18 @@ public class DestroyOnBouncy : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.tag == "Enemy") StartCoroutine(Hit(collision));
         if (!collision.gameObject.CompareTag("Wall")) return;
 
         _currentBouncy++;
 
         if (_currentBouncy >= _maxInBouncy) Destroy(gameObject);
+    }
+
+    IEnumerator Hit(Collision2D collision)
+    {
+        collision.gameObject.GetComponentInChildren<Animator>().SetTrigger("got_hit_trig");
+        yield return new WaitForSeconds(1);
+        collision.gameObject.GetComponent<Heal>().OnTakeDamage(1);
     }
 }
